98649188b7cb7479e3f3129af6dbb8585fc2e0b7
Kai Devrim <kkgithub@devrim.tech>
2020-08-28 10:51:24 -0700

    Removed copyrighted materials, added the spanish 2 textbook

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
 import requests
 
-currentImage = 1
-baseURL = "https://vtext-cdn.vhlcentral.com/vtext_descubre2ev1/ebook/m/img/page/landscape/0"
+currentImage = 350
+baseURL = "https://vtext-cdn.vhlcentral.com/vtext_descubre2ev2/ebook/data/swf/des2e_v2/0"
 
-while (currentImage > 0 and currentImage < 385):
+while (currentImage > 0 and currentImage < 392):
     if (currentImage > 0 and currentImage < 11):
-        baseURL = "https://vtext-cdn.vhlcentral.com/vtext_descubre2ev1/ebook/m/img/page/landscape/000"
-        betterURL = (baseURL + str(currentImage) + '.jpg')
-        print (baseURL + str(currentImage) + ".jpg")
+        baseURL = "https://vtext-cdn.vhlcentral.com/vtext_descubre2ev2/ebook/data/swf/des2e_v2/000"
+        betterURL = (baseURL + str(currentImage) + '.swf')
+        print(baseURL + str(currentImage) + ".swf")
         currentImage += 1
-        
-        
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'}
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'}
         r = requests.get(betterURL, headers=headers)
         print(r.status_code)
         print(r.headers)
-        open('./images/page' + str(currentImage) + '.jpg', 'wb').write(r.content)
+        open('./images/page' + str(currentImage) + '.swf', 'wb').write(r.content)
     elif (currentImage >= 11 and currentImage < 100):
-        baseURL = "https://vtext-cdn.vhlcentral.com/vtext_descubre2ev1/ebook/m/img/page/landscape/00"
-        betterURL = (baseURL + str(currentImage) + '.jpg')
-        print (baseURL + str(currentImage) + ".jpg")
+        baseURL = "https://vtext-cdn.vhlcentral.com/vtext_descubre2ev2/ebook/data/swf/des2e_v2/00"
+        betterURL = (baseURL + str(currentImage) + '.swf')
+        print(baseURL + str(currentImage) + ".swf")
         currentImage += 1
-        
-        
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'}
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'}
         r = requests.get(betterURL, headers=headers)
         print(r.status_code)
         print(r.headers)
-        open('./images/page' + str(currentImage) + '.jpg', 'wb').write(r.content)
+        open('./images/page' + str(currentImage) + '.swf', 'wb').write(r.content)
     elif (currentImage >= 100):
-        baseURL = "https://vtext-cdn.vhlcentral.com/vtext_descubre2ev1/ebook/m/img/page/landscape/0"
-        betterURL = (baseURL + str(currentImage) + '.jpg')
-        print (baseURL + str(currentImage) + ".jpg")
+        baseURL = "https://vtext-cdn.vhlcentral.com/vtext_descubre2ev2/ebook/data/swf/des2e_v2/0"
+        betterURL = (baseURL + str(currentImage) + '.swf')
+        print(baseURL + str(currentImage) + ".swf")
         currentImage += 1
-        
-        
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'}
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'}
         r = requests.get(betterURL, headers=headers)
         print(r.status_code)
         print(r.headers)
-        open('./images/page' + str(currentImage) + '.jpg', 'wb').write(r.content)
+        open('./images/page' + str(currentImage) + '.swf', 'wb').write(r.content)
 
     else:
         print("Did it work?")
